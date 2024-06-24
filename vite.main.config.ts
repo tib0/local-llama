ef02edcf5a11c6013ead52aa86e666f7b12172d3
Tib0 <4764729+tib0@users.noreply.github.com>
2024-06-24 05:08:33 +0200

    Add pre build minify renderer and main

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
+    minify: true,
     rollupOptions: {
       external: ["node-llama-cpp"],
     },
--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -3,5 +3,9 @@ import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  build: {
+    minify: true,
+    chunkSizeWarningLimit: 800,
+  },
   plugins: [react(), tsconfigPaths()],
 });
