5ee374fd907197ec59332a08c299db8435c40bae
Tib0 <4764729+tib0@users.noreply.github.com>
2024-07-02 15:12:17 +0200

    Update assets

--- src/frontend/lib/images.ts
+++ src/frontend/lib/images.ts
@@ -1,18 +1,14 @@
-import logo from "@assets/logo.png";
+import logo from "@assets/logo.webp";
 
 import blue from "@assets/blue.gif";
-import fire from "@assets/fire.gif";
-import gray from "@assets/gray.gif";
 import green from "@assets/green.gif";
-import pink from "@assets/pink.gif";
+import yellow from "@assets/yellow.gif";
 import red from "@assets/red.gif";
 
 export default {
   logo,
   blue,
-  fire,
-  gray,
+  yellow,
   green,
-  pink,
   red,
 };
