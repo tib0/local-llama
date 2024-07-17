92ef1e63bce66bf69cb45b754c64f837ab8fd7ad
Tib0 <4764729+tib0@users.noreply.github.com>
2024-07-17 16:02:44 +0200

    Remove default add shortcut

--- src/backend/windowsSquirelInstallEvent.ts
+++ src/backend/windowsSquirelInstallEvent.ts
@@ -1,43 +1,18 @@
-import path from "path";
-import { spawn as cpSpawn } from "child_process";
-
 export function handleSquirrelEvent() {
   if (process.argv.length === 1) {
     return false;
   }
-
-  const appFolder = path.resolve(process.execPath, "..");
-  const rootAtomFolder = path.resolve(appFolder, "..");
-  const updateDotExe = path.resolve(path.join(rootAtomFolder, "Update.exe"));
-  const exeName = path.basename(process.execPath);
-
-  const spawn = function (command, args) {
-    let spawnedProcess;
-
-    try {
-      spawnedProcess = cpSpawn(command, args, { detached: true });
-    } catch (error) {
-      console.error(error);
-    }
-
-    return spawnedProcess;
-  };
-
-  const spawnUpdate = function (args) {
-    return spawn(updateDotExe, args);
-  };
 
   const squirrelEvent = process.argv[1];
 
   switch (squirrelEvent) {
     case "--squirrel-install":
     case "--squirrel-updated":
-      spawnUpdate(["--createShortcut", exeName]);
       return true;
     case "--squirrel-uninstall":
-      spawnUpdate(["--removeShortcut", exeName]);
       return true;
     case "--squirrel-obsolete":
       return true;
   }
+  return false;
 }
